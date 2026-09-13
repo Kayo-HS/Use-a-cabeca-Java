c2f059ae8c15f7246b093b9de50d24bcdc519739
Kayo <kayohsantos0@gmail.com>
2026-09-12 21:54:58 -0300

    Deu certo, vou dormir

--- maiorNum.java
+++ maiorNum.java
@@ -8,7 +8,7 @@ public class maiorNum {
         
         List <Integer> numeros = new ArrayList<>();
 
-        int x = 0;
+        int x = 1;
 
         while(x < 6){
         System.out.println("==================");
@@ -16,18 +16,21 @@ public class maiorNum {
         System.out.println("==================");
         int numero = input.nextInt();
         numeros.add(numero);
+        x += 1;
         
-        int maior = numeros.get(0);
-
-        for (int numeroFor : numeros){
-        if (numero > maior ){
-            maior = numero;
-        }
-    }
-
+        
+        
     
-    System.out.println("Maior número é " + maior );
     };
+    int maior = Integer.MIN_VALUE;
+        
+    for(int i = 0; i < numeros.size(); i++){
+            if (numeros.get(i) > maior ){
+                maior = numeros.get(i);
+        }
+        }
+    System.out.println("Sua lista é " + numeros);
+    System.out.println("Maior número é " + maior);
     input.close();
     
     }
